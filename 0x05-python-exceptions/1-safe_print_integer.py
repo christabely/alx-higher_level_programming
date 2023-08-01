a86e87ab1c6badbd07ed1b36a801a7ac4aa853e8
Christabel Aidoo <christabelaidoo71@gmail.com>
2023-08-01 01:33:37 -0700

    upade_exectpions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-import sys
-def safe_print_integer_err(value):
+def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except (Exception) as err:
-        sys.stderr.write('Exception: ' + str(err) + '\n')
-        return (False)
-    return (True)
+        return True
+    except (ValueError, TypeError):
+        return False
--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except (Exception) as err:
-        sys.stderr.write('Exception: ' + str(err) + '\n')
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (False)
-    return (True)
